618ea309f2ba2505d71d17394ff6d9cc8780acf7
Timilehin Abodunrin <ezekielabodunrin@gmail.com>
2025-07-15 18:27:50 +0100

    My terraform module

--- variable.tf
+++ variable.tf
@@ -6,15 +6,6 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-0841b1152f02fa85e"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
 variable "key_name" {
   default = "cba_keypair"
 }
